be03bd7515e82c417b4ac84ec091169c90ae0526
Basil1361 <trainerbasil69@gmail.com>
2025-09-03 16:32:13 +0800

    Update

--- Cubie/Python/BasicLogic copy/If_Else/Basic2/Factors.py
+++
@@ -1,8 +0,0 @@
-def factors(n):
-    for i in range(1,n+1):
-        if n % i == 0:
-            print (i)
-        else:
-            continue
-        
-factors(6)
--- Cubie/Python/BasicLogic copy/List/Basic2/Listing.py
+++
@@ -1,6 +0,0 @@
-# New abuse: 
-# use list to create functions 
-
-
-div5and3 = [x for x in range(100) if x % 3 == 0 and x % 5 == 0]
-print(div5and3)
