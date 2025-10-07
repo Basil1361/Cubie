0d896908fe78bbd408390b23e93c9bed4eba075f
Basil1361 <trainerbasil69@gmail.com>
2025-10-07 18:11:16 +0800

    Update

--- Cubie/Python/Data Wranging/File_import.py
+++
@@ -1,7 +0,0 @@
-with open('Test.file.txt') as f:
-# f = open('Test.file.txt') = same
-    print(f.read())
-    
-# https://www.youtube.com/watch?v=BRrem1k3904
-
-
--- Cubie/Python/Data Wranging/Import_Text.py
+++
@@ -1,8 +0,0 @@
-# pip install requests
-import requests
-
-url = "https://jsonplaceholder.typicode.com/todos"
-resp = requests.get(url, timeout=15)
-data = resp.json()
-
-print(data)
